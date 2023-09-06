2095967b368d126b94b9a41190ead2ff7bc17a9b
MantonySLX <144179385+MantonySLX@users.noreply.github.com>
2023-09-06 06:00:53 -0500

    Update pardot_oauth_flask_app.py

--- pardot_oauth_flask_app.py
+++ pardot_oauth_flask_app.py
@@ -92,7 +92,7 @@ def find_qualified_prospects():
     response = requests.get(
         "https://pi.pardot.com/api/v5/objects/visitorActivities",
         headers=headers,
-        params={"query": "type": "Page View"}
+        params={"type": "Page View"}
     )
     visitor_activities_data = response.json()
 
