1b9af84db3632efa0a1a3a6057841099b0883bc2
MantonySLX <144179385+MantonySLX@users.noreply.github.com>
2023-09-12 16:43:32 -0500

    Update pardot_oauth_flask_app.py

--- pardot_oauth_flask_app.py
+++ pardot_oauth_flask_app.py
@@ -51,13 +51,11 @@ def logout():
     session.clear()
     return redirect("/")
 
-import requests
-
 def get_prospects_by_url(access_token):
     api_endpoint = "https://pi.pardot.com/api/v5/objects/visitor-page-views"
     headers = {
         "Authorization": f"Bearer {access_token}",
-        "Pardot-Business-Unit-Id": "0Uv5A000000PAzxSAG",  # Business Unit ID
+        "Pardot-Business-Unit-Id": "0Uv5A000000PAzxSAG",
     }
     params = {
         "fields": "id,url,title,createdAt,visitorId,campaignId,visitId,durationInSeconds,salesforceId"
@@ -67,19 +65,11 @@ def get_prospects_by_url(access_token):
     
     if response.status_code == 200:
         data = response.json()
-        # Filter the results where URL is https://saleslabx.com/demo-page/
         filtered_data = [item for item in data.get('data', []) if item.get('url') == 'https://saleslabx.com/demo-page/']
-        # Extract the visitor IDs from the filtered data
         visitor_ids = [item.get('visitorId') for item in filtered_data]
-        return visitor_ids[:20]  # Limit to first 20
+        return visitor_ids[:20]
     else:
         return None
-
-
-import xml.etree.ElementTree as ET
-# ... (your existing imports)
-
-# ... (your existing setup and routes)
 
 @app.route("/prospects_from_opportunities")
 def prospects_from_opportunities():
@@ -88,15 +78,13 @@ def prospects_from_opportunities():
         return jsonify({"error": "Not authenticated"}), 401
     
     try:
-        # Calculate the date 6 months ago
         six_months_ago = datetime.now() - timedelta(days=180)
         six_months_ago_str = six_months_ago.strftime('%Y-%m-%d %H:%M:%S')
         
-        # Fetch Opportunities created in the last 6 months using Pardot API
         api_endpoint = "https://pi.pardot.com/api/opportunity/version/4/do/query"
         headers = {
             "Authorization": f"Bearer {access_token}",
-            "Pardot-Business-Unit-Id": "0Uv5A000000PAzxSAG"  # Your Business Unit ID
+            "Pardot-Business-Unit-Id": "0Uv5A000000PAzxSAG"
         }
         params = {
             "created_after": six_months_ago_str,
@@ -115,7 +103,7 @@ def prospects_from_opportunities():
                     for prospect in opportunity.findall(".//prospect"):
                         prospect_ids.append(prospect.find("id").text)
                 
-                session['prospect_ids'] = prospect_ids  # Storing in session
+                session['prospect_ids'] = prospect_ids
                 
                 return jsonify({"prospect_ids": prospect_ids})
             except ET.ParseError:
@@ -138,7 +126,7 @@ def visited_urls_by_prospects():
     
     headers = {
         "Authorization": f"Bearer {access_token}",
-        "Pardot-Business-Unit-Id": "0Uv5A000000PAzxSAG"  # Your Business Unit ID
+        "Pardot-Business-Unit-Id": "0Uv5A000000PAzxSAG"
     }
     
     for prospect_id in prospect_ids:
@@ -149,28 +137,17 @@ def visited_urls_by_prospects():
         api_endpoint = "https://pi.pardot.com/api/v5/objects/visitor-page-views"
         response = requests.get(api_endpoint, headers=headers, params=params)
         
-        for prospect_id in prospect_ids:
-    params = {
-        "fields": "id,url,title,createdAt,visitorId,campaignId,visitId,durationInSeconds,salesforceId",
-        "prospect_id": prospect_id
-    }
-    api_endpoint = "https://pi.pardot.com/api/v5/objects/visitor-page-views"
-    response = requests.get(api_endpoint, headers=headers, params=params)
-    
-    if response.status_code == 200:
-        data = response.json()
-        urls = [item.get('url') for item in data.get('data', [])]
-        visited_urls_by_prospects[prospect_id] = urls
-    else:
-        error_detail = f"Failed to fetch data for prospect ID {prospect_id}. "
-        error_detail += f"Status code: {response.status_code}, "
-        error_detail += f"Raw response: {response.text}"
-        return jsonify({"error": error_detail}), 400
+        if response.status_code == 200:
+            data = response.json()
+            urls = [item.get('url') for item in data.get('data', [])]
+            visited_urls_by_prospects[prospect_id] = urls
+        else:
+            error_detail = f"Failed to fetch data for prospect ID {prospect_id}. "
+            error_detail += f"Status code: {response.status_code}, "
+            error_detail += f"Raw response: {response.text}"
+            return jsonify({"error": error_detail}), 400
 
-    
     return jsonify({"visited_urls_by_prospects": visited_urls_by_prospects})
-
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
